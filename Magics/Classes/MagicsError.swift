8d909173a71c1632a121a319d925245c4923b75f
Nikita Arkhipov <narhipov92@gmail.com>
2018-11-09 17:04:32 +0300

    Changed MagicsError access level

--- Magics/Classes/MagicsError.swift
+++ Magics/Classes/MagicsError.swift
@@ -12,14 +12,25 @@ public class MagicsError: Error {
     let code: Int
     let message: String
 
-    init(code: Int, message: String) {
+    public init(code: Int, message: String) {
         self.code = code
         self.message = message
     }
     
-    static func fromError(_ error: Error?) -> MagicsError?{
+    public static func fromError(_ error: Error?) -> MagicsError?{
         guard let error = error as NSError? else { return nil }
         return MagicsError(code: error.code, message: error.description)
+    }
+    
+    public static func fromJSON(_ json: MagicsJSON?) -> MagicsError?{
+        if let json = json,
+            let status = json["status"]?.string,
+            let code = json["code"]?.int,
+            let message = json["message"]?.string,
+            status == "error"{
+            return MagicsError(code: code, message: message)
+        }
+        return nil
     }
 }
 
